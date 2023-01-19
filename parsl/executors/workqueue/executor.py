a9869b627f50a0f15df84f50f8d42446b12ab1cb
Ben Clifford <benc@hawaga.org.uk>
2023-01-19 10:50:43 +0000

    Form WQ worker_command in __init__ to make type visible to mypy
    TODO: this breaks: the port hasn't been chosen by this time - it happens at startup

--- parsl/executors/workqueue/executor.py
+++ parsl/executors/workqueue/executor.py
@@ -292,6 +292,8 @@ class WorkQueueExecutor(BlockProviderExecutor, putils.RepresentationMixin):
         if self.init_command != "":
             self.launch_cmd = self.init_command + "; " + self.launch_cmd
 
+        self.worker_command = self._construct_worker_command()
+
     def _get_launch_command(self, block_id):
         # this executor uses different terminology for worker/launch
         # commands than in htex
@@ -645,7 +647,6 @@ class WorkQueueExecutor(BlockProviderExecutor, putils.RepresentationMixin):
         """
         # Start scaling in/out
         logger.debug("Starting WorkQueueExecutor with provider: %s", self.provider)
-        self.worker_command = self._construct_worker_command()
         self._patch_providers()
 
         if hasattr(self.provider, 'init_blocks'):
